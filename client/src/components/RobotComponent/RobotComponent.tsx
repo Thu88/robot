a056a9edfb581fe597156f67f1f3e1f85f190eb0
Thu88 <jublen@protonmail.com>
2022-03-25 16:08:07 +0100

    Make img w and h larger

--- client/src/components/RobotComponent/RobotComponent.tsx
+++ client/src/components/RobotComponent/RobotComponent.tsx
@@ -11,19 +11,19 @@ const RobotComponent = ({ robot }: Props): JSX.Element => {
     useEffect(() => {
         switch (robot.direction) {
             case DIRECTION.North:
-                setImgDirection(<img src={"/img/robotnorth.svg"} />)
+                setImgDirection(<img width="50px" height="50px" src={"/img/robotnorth.svg"} />)
                 break;
             
             case DIRECTION.East:
-                setImgDirection(<img src="/img/roboteast.svg" />)
+                setImgDirection(<img width="50px" height="50px" src="/img/roboteast.svg" />)
                 break;
 
             case DIRECTION.South:
-                setImgDirection(<img src="/img/robotsouth.svg" />)
+                setImgDirection(<img width="50px" height="50px" src="/img/robotsouth.svg" />)
                 break;
 
             case DIRECTION.West:
-                setImgDirection(<img src="/img/robotwest.svg" />)
+                setImgDirection(<img width="50px" height="50px" src="/img/robotwest.svg" />)
                 break;
         }
     }, []);
