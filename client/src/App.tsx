9d7586c7983f35babd00468c0484ac1458b537fa
Thu88 <jublen@protonmail.com>
2022-04-08 16:43:34 +0200

    Show room component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import RobotComponent from './components/RobotComponent';
+import RoomComponent from './components/RoomComponent';
+import SquareComponent from './components/SquareComponent';
+import Robot from './models/Robot';
+import Square from './models/Square';
 
 function App() {
+  const square: Square = new Square(1, 1);
+  square.active = true;
   return (
     <div className="App">
-      
+
+      <RoomComponent />
     </div>
   );
 }
