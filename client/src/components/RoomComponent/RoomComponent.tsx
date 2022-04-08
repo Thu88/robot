a38d26d6fd962398095597691f445b3e3d7fb14c
Thu88 <jublen@protonmail.com>
2022-04-08 16:43:20 +0200

    Add input fields to room component

--- client/src/components/RoomComponent/RoomComponent.tsx
+++ client/src/components/RoomComponent/RoomComponent.tsx
@@ -1,15 +1,20 @@
-import { useEffect, useState } from "react"
+import { FormEvent, useEffect, useState } from "react"
 import Room from "../../models/Room"
 import SquareComponent from "../SquareComponent"
 import { ClassNameMap, makeStyles } from "@mui/styles";
-import { Box, Paper } from "@mui/material"
+import { Box, Button, FormControl, InputLabel, MenuItem, Paper, Select, TextField } from "@mui/material"
 import { DIRECTION } from "../../models/Robot";
+import { padding } from "@mui/system";
+import React from "react";
 
 const useStyles = makeStyles(() => ({
     container: {
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'center',
+        alignItems: 'center',
+        height: '90vh',
+        gridGap: '100px'
     },
     row: {
       display: 'flex',
@@ -17,19 +22,95 @@ const useStyles = makeStyles(() => ({
     },
     room: {
       width: 'fit-content'  
+    },
+    inputsContainer: {
+        padding: '50px',
+    },
+    startInputsContainer: {
+        width: '500px',
+        display: 'flex',
+        flexDirection: 'column',
+        gridGap: '30px',
+        alignItems: 'center',
+        justifyContent: 'center'
+    
+    },
+    inputs: {
+       width: '300px'
+    },
+    input: {
     }
 }))
 
 const RoomComponent = () => {
     const classes: ClassNameMap = useStyles();
     const [room, setRoom] = useState<Room>(new Room(10))
+    const [startX, setStartX] = useState<number>(0);
+    const [startY, setStartY] = useState<number>(0);
+    const [startDirection, setStartDirection] = useState<number | String>(0);
+
+    const handleChange = ((e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, setState: Function) => {
+       setState(e.target.value);
+    })
+    const xyValidation = (val: String): boolean => {
+        val.split('').forEach((s) => {
+            console.log(s)
+            if (!(s >= '0' && s <= '9')) {
+                return false;
+            }
+        });
+        return true;
+    }
     room.setRobotPositionAndDirection(0, 0, DIRECTION.East);
     useEffect(() => {
        
     }, [])
     return (
         <Box className={classes.container}>
+            <Paper className={classes.inputsContainer}>
+                <Box className={classes.startInputsContainer}>
+                    <Box className={classes.inputs}>
+                        <FormControl fullWidth>
+                            <TextField type="number" value={startX} onChange={(e) => {handleChange(e, setStartX)}} className={classes.input} label="Enter the robot's start x coordinate" />
+                        </FormControl>
+                    </Box>
+
+                    <Box className={classes.inputs}>
+                        <FormControl fullWidth>
+                            <TextField className={classes.input} value={startY} onChange={(e) => {handleChange(e, setStartY)}} label="Enter the robot's start y coordinate" />
+                        </FormControl>
+                    </Box>
+
+                    <Box className={classes.inputs}>
+                        <FormControl fullWidth>
+                            <InputLabel id="select-start-direction">Start direction</InputLabel>
+                            <Select
+                                labelId="select-start-direction"
+                                id="demo-simple-select"
+                                className={classes.input}
+                                label="Start direction"
+                                type="number"
+                                value={startDirection}
+                                onChange={(e) => {
+                                    setStartDirection(e.target.value);
+                                }}
+                            >
+                                <MenuItem value={DIRECTION.North}>North</MenuItem>
+                                <MenuItem value={DIRECTION.East}>East</MenuItem>
+                                <MenuItem value={DIRECTION.South}>South</MenuItem>
+                                <MenuItem value={DIRECTION.West}>West</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Box>
+                    <Box className={classes.inputs}>
+                        <FormControl fullWidth>
+                            <Button className={classes.input} variant="contained">Enter</Button>
+                        </FormControl>
+                    </Box>
+                </Box>
+            </Paper>
             <Paper className={classes.room}>
+                {/* Render the squares */}
                 {room.squares.map((sqrow, index)=> {
                     return (
                         <Box className={classes.row} key={`sqrow${index}`}>
@@ -42,6 +123,7 @@ const RoomComponent = () => {
                         </Box>
                     )
                 }).reverse()}
+
             </Paper>
         </Box>
     )
